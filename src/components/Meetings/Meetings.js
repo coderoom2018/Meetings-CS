cc16bd6c648dd9f85628d4118d439e1d3be7d2f7
LeeSeungJae <coderoom2018@gmail.com>
2019-02-28 20:06:23 +0900

    add searchMeetings function and search input tag

--- src/components/Meetings/Meetings.js
+++ src/components/Meetings/Meetings.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Meetings.css';
 import MeetingContainer from './MeetingsContainer';
 import module from '../../utilities';
+import SearchMeetings from './SearchMeetings';
 
 export default class Meetings extends Component {
   state = {
@@ -20,15 +21,24 @@ export default class Meetings extends Component {
     this.setState({ data })
   }
 
+  _searchMeetings = async(target) => {
+    console.log("type: ", typeof target, "target: ", target)
+    const searchMeetings = await module.searchMeeings;
+    const data = await searchMeetings("meetingsData", `${target}`)
+
+    console.log("searchData: ", data)
+    this.setState({ data })
+  }
+
   render() {
     // console.log("module: ", module)
 
     return (
       <div id="meetings_content">
         <h1>Meetings</h1>
-        <div>
+          <SearchMeetings _searchMeetings={this._searchMeetings}/>
           <MeetingContainer data={this.state.data}/>
-        </div>
+        
       </div>
     )
   }
