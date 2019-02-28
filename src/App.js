5aaab112792c72050403430053bde1359d5b3ecb
LeeSeungJae <coderoom2018@gmail.com>
2019-02-28 20:07:49 +0900

    add tab change between meetings and cs component by clicking tabname button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
-import Head from './Head';
+import React, { Component } from "react";
+import "./App.css";
+import Head from "./Head";
 // import { Link } from 'react-router'
-import Meetings from './components/Meetings/Meetings';
-import Cs from './components/Cs/Cs';
+import Meetings from "./components/Meetings/Meetings";
+import Cs from "./components/Cs/Cs";
 
 class App extends Component {
+  state = {
+    tabName: "Meetings"
+  };
+
+  _clickHandler_changeTabName = (tabName) => {
+    this.setState({ tabName })
+  };
+
   render() {
     return (
       <div className="App">
-        <Head />
+        <Head _clickHandler_changeTabName={this._clickHandler_changeTabName}/>
         <div>
-          <Meetings />
-          <Cs />
+          {this.state.tabName === "Meetings" ? <Meetings /> : <Cs />}
         </div>
       </div>
     );
--- src/Head.js
+++ src/Head.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
 import './Head.css'
-// import { Link } from
+
+// const styleHeader = {
+//   backgroundColor: '#fff',
+//   padding: '10px',
+//   textDecoration: 'none',
+//   position: 'fixed',
+//   width: '100%',
+//   zIndex: '2',
+//   top: '0',
+//   height: '30px',
+//   boxShadow: '0px 1px 7px 0px rgba(0, 0, 0, 0.4)',
+//   display: 'flex',
+//   transition: 'top 0.2s ease-in-out',
+// };
+
+
 
 export default class Head extends Component {
 
+  _changeTabName = (event) => {
+    const tabNamge = event.target.value
+
+    console.log(tabNamge);
+    this.props._clickHandler_changeTabName(tabNamge)
+  }
+
   render() {
+      
     return (
-      <div id="head_content">
-        <h1>Head</h1>
+      <div 
+        id="header" 
+        >
         <div>
-          <button>Meetings</button>
-          <button>Cs</button>
+          <button onClick={this._changeTabName} value="Meetings">Meetings</button>
+          <button onClick={this._changeTabName} value="Cs">Cs</button>
         </div>
       </div>
     )
