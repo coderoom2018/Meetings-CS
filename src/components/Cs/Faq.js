971411afc715e7c3912c6ce7e9203e5d96c085db
LeeSeungJae <coderoom2018@gmail.com>
2019-02-28 16:35:04 +0900

    display or none by clicking title

--- src/components/Cs/Faq.js
+++ src/components/Cs/Faq.js
@@ -6,18 +6,27 @@ export default class Faq extends Component {
     super(props);
   }
 
+  _clickHandler_changeDisplay = (event) => {
+    let display = document.getElementById(`${this.props.faq.title}`).style.display;
+
+    display === "block"
+      ? document.getElementById(`${this.props.faq.title}`).style.display = "none" 
+      : document.getElementById(`${this.props.faq.title}`).style.display = "block"
+    
+  }
+
   render() {
     const faq = this.props.faq
 
     return (
       <div id="faq_content">
-        <div className="faq_title">
+        <div className="faq_title" onClick={this._clickHandler_changeDisplay}>
           <span className="faq_title-q" >Q.</span>
           <span className="faq_title-tapName">{`[${faq.category}]`}</span>
           <span className="faq_title-title">{faq.title}</span>
         </div>
 
-        <div className="faq_description">
+        <div id={faq.title} className="faq_description" style={{display:"block"}}>
           <div>
             <p>{faq.description}</p>
           </div>
