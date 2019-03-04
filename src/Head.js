5c9fd27d832711045bbcdd7336ae9fcd0bd98b52
LeeSeungJae <coderoom2018@gmail.com>
2019-03-04 15:36:13 +0900

    add hide navbar by scrolldown

--- src/Head.js
+++ src/Head.js
@@ -1,24 +1,58 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import './Head.css'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Head.css";
+import classnames from "classnames";
 
 export default class Head extends Component {
+  constructor(props) {
+    super(props);
 
-  _changeTabName = (event) => {
-    const tabNamge = event.target.value
-
-    this.props._clickHandler_changeTabName(tabNamge)
+    this.state = {
+      prevScrollpos: window.pageYOffset,
+      visible: true
+    };
   }
 
+  _changeTabName = event => {
+    const tabNamge = event.target.value;
+    this.props._clickHandler_changeTabName(tabNamge);
+  };
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = () => {
+    const { prevScrollpos } = this.state;
+    const currentScrollPos = window.pageYOffset;
+    let visible;
+
+    if (prevScrollpos > currentScrollPos) {
+      visible = true;
+    } else if (prevScrollpos < currentScrollPos) {
+      visible = false;
+    }
+
+    this.setState({
+      prevScrollpos: currentScrollPos,
+      visible
+    });
+  };
+
   render() {
-      
     return (
-      <div id="header">
-        <div>
-          <Link to="/"><button>Meetings</button></Link>
-          <Link to="cs"><button>Cs</button></Link>
-        </div>
+      <div
+        className={classnames("navbar2", {
+          "navbar--hidden2": !this.state.visible
+        })}
+      >
+        <Link to="/"><button>Meetings</button></Link>
+        <Link to="cs"><button>Cs</button></Link>
       </div>
-    )
+    );
   }
 }
